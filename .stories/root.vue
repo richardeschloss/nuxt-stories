48db29d6e8313b27f662b431957e91400382b9e0
Richard Schloss <richard.e.schloss@gmail.com>
2019-10-29 23:25:52 -0700

    Added link to nav back to app

--- .stories/root.vue
+++ .stories/root.vue
@@ -2,6 +2,11 @@
   <div>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Stories</a>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="/">Back to APP</a>
+        </li>
+      </ul>
     </nav>
     <div class="container-fluid">
       <div class="row">
