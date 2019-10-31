f5f9f278aa2175734ca6cceec4fb07aea3ccf002
Richard Schloss <richard.e.schloss@gmail.com>
2019-10-30 22:43:04 -0700

    Documented nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,9 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/richardeschloss/nuxt-stories
-    ['modules/stories.module', { forceBuild: true }]
+    // To force include in static site, use:
+    // ['modules/stories.module', { forceBuild: true }]
+    'modules/stories.module' // Enabled for dev mode only
   ],
   /*
    ** Nuxt.js modules
