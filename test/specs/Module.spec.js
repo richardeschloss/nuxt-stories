cd597b83a4a2829a5a59d7a9d5974473396dad70
Richard Schloss <richard.e.schloss@gmail.com>
2019-10-31 22:24:15 -0700

    Updated automated tests

--- test/specs/Module.spec.js
+++ test/specs/Module.spec.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import { readdirSync } from 'fs'
 import path from 'path'
 import test from 'ava'
 import NuxtStoriesMod from '@/modules/stories.module'
@@ -6,23 +7,47 @@ import NuxtStoriesMod from '@/modules/stories.module'
 const srcDir = path.resolve('.')
 
 test('Story routes created ok', (t) => {
+  const storiesDir = '.stories'
+  const forceBuild = true
+  const modOptions = {
+    forceBuild,
+    storiesDir
+  }
+  const files = readdirSync(storiesDir)
+  const storiesRoot = files[0]
   return new Promise((resolve) => {
+    let doneCnt = 0
+    const expCnt = 2
+    function handleDone() {
+      if (++doneCnt === expCnt) resolve()
+    }
     const sampleRoutes = []
     const modContainer = {
       extendRoutes(fn) {
         fn(sampleRoutes, path.resolve)
         const storyRoute = sampleRoutes[0]
         console.log('storyRoute', storyRoute)
-        t.is(storyRoute.name, '.stories')
-        t.is(storyRoute.path, '/.stories')
-        t.is(storyRoute.component, path.resolve(srcDir, '.stories/index.vue'))
-        t.is(storyRoute.chunkName, '.stories/index')
+        t.is(storyRoute.name, storiesDir)
+        t.is(storyRoute.path, `/${storiesDir}`)
+        t.is(
+          storyRoute.component,
+          path.resolve(srcDir, `${storiesDir}/${storiesRoot}.vue`)
+        )
+        t.is(storyRoute.chunkName, `${storiesDir}/${storiesRoot}`)
         t.truthy(storyRoute.children)
         t.true(storyRoute.children.length > 0)
-        resolve()
+        handleDone()
       }
     }
     const simpleNuxt = {
+      addPlugin({ src, fileName, options }) {
+        console.log('options', options)
+        t.is(src, path.resolve(srcDir, 'modules/stories.plugin.js'))
+        t.is(fileName, 'nuxt-stories.js')
+        t.is(options.storiesDir, storiesDir)
+        t.is(options.storiesAnchor, storiesDir)
+        handleDone()
+      },
       options: {
         srcDir,
         modules: []
@@ -38,6 +63,6 @@ test('Story routes created ok', (t) => {
       },
       registerMod: NuxtStoriesMod
     }
-    simpleNuxt.registerMod({ forceBuild: true })
+    simpleNuxt.registerMod(modOptions)
   })
 })
