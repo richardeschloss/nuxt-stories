36096e87eaf5e0f7521972ef3e7b6eed4b97786f
Richard Schloss <richard.e.schloss@gmail.com>
2020-01-20 15:45:40 -0700

    Added markdown examples

---
+++ .stories/index/Markdown.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div v-show="false" v-markdown>
+  # My example
+
+  ## Header 2
+
+  * Bullet 1
+  * Bullet 2
+  
+  ** Something more
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- modules/stories.plugin.js
+++ modules/stories.plugin.js
@@ -1,3 +1,15 @@
+import DOMPurify from 'dompurify'
+import marked from 'marked'
+import Vue from 'vue'
+
+Vue.directive('markdown', {
+  inserted(el) {
+    const compiled = marked(el.textContent.trim())
+    el.innerHTML = DOMPurify.sanitize(compiled)
+    el.style.display = 'block'
+  }
+})
+
 function nuxtStories() {
   // eslint-disable-next-line
   const pluginOptions = <%= JSON.stringify(options) %>
