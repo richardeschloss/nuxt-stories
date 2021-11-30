7112b2f22cd70c759f86fb4b28f2aa83cfd0a212
Richard Schloss <richard.e.schloss@gmail.com>
2021-11-30 15:14:28 -0500

    Added tests for db client too.

--- test/specs/DB.spec.js
+++ test/specs/DB.spec.js
@@ -1,29 +1,40 @@
-import { unlinkSync } from 'fs'
+import { existsSync, readFileSync, unlinkSync } from 'fs'
 import { resolve as pResolve } from 'path'
-import { promisify } from 'util'
-import Glob from 'glob'
 import ava from 'ava'
 import DB from '#root/lib/db.js'
+import DBClient from '#root/lib/db.client.js'
 
-const glob = promisify(Glob)
 const srcDir = pResolve('.')
 const storiesDir = 'stories'
 const lang = 'en'
 const basePath = pResolve(srcDir, storiesDir, lang)
 const pattern = '**/*.md'
-let db
+let db, dbClient
 
 const { serial: test, before } = ava
+const dbFile = pResolve('./stories/stories.db')
+const fetched = []
+
+global.fetch = async function(url) {
+  fetched.push(url)
+  return {
+    json() {
+      return JSON.parse(readFileSync(dbFile, { encoding: 'utf-8' }))
+    }
+  }
+}
 
 before(async () => {
-  unlinkSync(pResolve('./stories/stories.db'))
-  db = await DB({ autosave: false })
+  if (existsSync(dbFile)) {
+    unlinkSync(dbFile)
+  }
+  db = await DB({ srcDir, autosave: false })
 })
 
 test('Init from FS', async (t) => {
-  const files = await await glob(pResolve(basePath, pattern))
-  await db.initFromFS(files, srcDir, storiesDir, lang)
+  await db.initFromFS(pResolve(srcDir, storiesDir, '**/*.md'))
   const items = db.find({})
+  const fnd = await db.search('HOLA?', 'es')
   t.true(items.length > 0)
 
   const firstItem = db.findOne({})
@@ -35,8 +46,28 @@ test('Init from FS', async (t) => {
   t.true(items2.length > 0)
 })
 
-test('Search', async (t) => {
+test('Init (client-side)', async (t) => {
+  dbClient = DBClient()
+  const cnt = await dbClient.load()
+  t.true(cnt > 0)
+})
+
+test('Search (server-side)', async (t) => {
   const hits = await db.search('Nice')
+  t.true(hits.length > 0)
+  hits.forEach((hit) => { 
+    t.truthy(hit.labels)
+    t.truthy(hit.href)
+    t.truthy(hit.content)
+    t.truthy(hit.preview)
+  })
+
+  const hits2 = await db.search('!HOLA', 'es')
+  t.is(hits2[0].content,  '# !HOLA! from Story1 in Spanish')
+})
+
+test('Search (client-side)', async (t) => {
+  const hits = await dbClient.search('Nice')
   t.true(hits.length > 0)
   hits.forEach((hit) => { 
     t.truthy(hit.labels)
