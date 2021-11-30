d5e62d3fd14993a843cd7630c0bf593e60bceb5f
Richard Schloss <richard.e.schloss@gmail.com>
2021-11-29 23:35:00 -0500

    Tested server-side db code to 100% cov

--- test/specs/DB.spec.js
+++ test/specs/DB.spec.js
@@ -1,3 +1,4 @@
+import { unlinkSync } from 'fs'
 import { resolve as pResolve } from 'path'
 import { promisify } from 'util'
 import Glob from 'glob'
@@ -15,14 +16,18 @@ let db
 const { serial: test, before } = ava
 
 before(async () => {
+  unlinkSync(pResolve('./stories/stories.db'))
   db = await DB({ autosave: false })
 })
 
-test.only('Init from FS', async (t) => {
+test('Init from FS', async (t) => {
   const files = await await glob(pResolve(basePath, pattern))
   await db.initFromFS(files, srcDir, storiesDir, lang)
   const items = db.find({})
   t.true(items.length > 0)
+
+  const firstItem = db.findOne({})
+  t.is(firstItem.href, items[0].href)
 
   // Test persistence
   const db2 = await DB({})
@@ -42,10 +47,52 @@ test('Search', async (t) => {
 })
 
 test('Update One', async (t) => {
-  // console.log(db.find())
-  await db.upsert()
+  await db.updateContent({ 
+    href: '/stories/en/Examples/Example2' ,
+    content: 'new content'
+  })
   const db2 = await DB({})
-  console.log(db2.findOne({ href: '/stories/en/Examples/Example2' }))
-  
-  t.pass()
+  const fnd = db2.findOne({ href: '/stories/en/Examples/Example2' })
+  t.is(fnd.content, 'new content')
+})
+
+test('Add Story', async (t) => {
+  const oldCnt = db.cnt()
+  await db.addStory({
+    href: '/stories/en/Something/New',
+    content: 'A new story',
+    storiesDir: 'stories', 
+    lang: 'en'
+  })
+  t.is(oldCnt + 1, db.cnt())
+  const fnd = db.findOne({ href: '/stories/en/Something/New' })
+  t.is(fnd.href, '/stories/en/Something/New')
+
+  await db.addStory({
+    href: '/stories/en/Something/New',
+    content: 'A new story',
+    storiesDir: 'stories', 
+    lang: 'en'
+  })
+  t.is(oldCnt + 1, db.cnt())
+})
+
+test('Rename Story', async (t) => {
+  await db.renameStory({
+    oldHref: '/stories/en/Something/New',
+    href: '/stories/en/Changed'
+  })
+  const fnd = await db.findOne({ href: '/stories/en/Changed' })
+  const fnd2 = await db.findOne({ href: '/stories/en/Something/New' })
+  t.is(fnd.href, '/stories/en/Changed')
+  t.is(fnd2, null)
+})
+
+test('Remove Story', async (t) => {
+  await db.removeStory({ href: '/stories/en/Changed'})
+  const fnd = await db.findOne({ href: '/stories/en/Changed' })
+  t.is(fnd, null)
+  const oldCnt = db.cnt()
+  await db.removeStory({ href: '/stories/en/NotExist'})
+  t.is(oldCnt, db.cnt())
 })
