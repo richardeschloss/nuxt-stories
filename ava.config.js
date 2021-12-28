019cef9ba2b0f90fe99b3a3f5b65ce022b6de7d3
Richard Schloss <richard.e.schloss@gmail.com>
2021-12-28 01:04:18 -0500

    Updated config

--- ava.config.js
+++ ava.config.js
@@ -1,22 +1,18 @@
 export default {
   serial: true,
   files: [
-    'test/specs/DB.spec.js',
+    // 'test/specs/DB.spec.js',
+    // 'test/specs/Module.spec.js',
+    // 'test/specs/Plugin.spec.js'
     // 'test/specs/Fetch.spec.js',
     // 'test/specs/Import.spec.js',
-    // 'test/specs/IO.spec.js',
+    'test/specs/IO.spec.js'
     // 'test/specs/Markdown.spec.js',
-    // 'test/specs/Module.spec.js',
-    // 'test/specs/Plugin.spec.js',
-    // 'test/specs/PluginRegister.spec.js',
-    
-    // 'test/specs/Postinstall.spec.js',
     // 'test/specs/Storage.spec.js',
     // 'test/specs/VuexModule.spec.js',
     // 'test/specs/StoryFactory.spec.js',
 
-    // 'test/e2e/Hello.js'
-    // 'test/e2e/Root.js', // Runs after DB.spec which initializes db
+    // 'test/e2e/Root.js'
     // 'test/e2e/Search.js'
     // 'test/e2e/ModeSelect.js'
   ],
