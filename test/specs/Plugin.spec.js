7285c65a23b4a59f6f0b22e7855fd33e578829a3
Richard Schloss <richard.e.schloss@gmail.com>
2021-12-27 22:57:36 -0500

    Updated plugin tests

--- test/specs/Plugin.spec.js
+++ test/specs/Plugin.spec.js
@@ -1,32 +1,119 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable node/no-callback-literal */
+import { readFileSync } from 'fs'
 import ava from 'ava'
 import { pluginCtx, pluginDef } from '../utils/plugin.js'
+
 // @ts-ignore
 import Plugin from '#root/lib/plugin.js'
 
 const { serial: test } = ava
 
-test('Plugin (dynamic host)', async (t) => {
+let fetched
+// eslint-disable-next-line require-await
+global.fetch = async function (url) {
+  fetched = url
+  return {
+    json () {
+      const db = readFileSync('./stories/stories.db', { encoding: 'utf-8' })
+      return JSON.parse(db)
+    }
+  }
+}
+
+test('Plugin (dynamic host)', (t) => {
+  const routes = []
   const ctx = pluginCtx()
   Object.assign(ctx, {
-    $config: {
-      nuxtStories: {
-        storiesDir: 'stories',
-        lang: 'en'
-      }
+    nuxt2Context: {
+      app: {
+        router: {
+          beforeEach (cb) {
+            // _beforeEachCb = cb
+            cb(
+              {
+                path: '/stories/en/',
+                params: {}
+              },
+              null,
+              () => {}
+            )
+            t.is(routes.length, 0)
+
+            cb(
+              {
+                path: '/stories',
+                params: {}
+              },
+              null,
+              () => {}
+            )
+            t.is(routes[0], '/stories/en/')
+
+            cb(
+              {
+                path: '/stories/en',
+                params: {}
+              },
+              null,
+              () => {}
+            )
+            t.is(routes[1], '/stories/en/')
+
+            cb(
+              {
+                path: '/stories/',
+                params: {}
+              },
+              null,
+              () => {}
+            )
+            t.is(routes[2], '/stories/en/')
+          },
+          push (route) {
+            routes.push(route)
+          }
+        }
+      },
+      $config: {
+        nuxtStories: {
+          storiesAnchor: 'stories',
+          storiesDir: 'stories',
+          lang: 'en'
+        }
+      },
+      store: ctx.store
     }
   })
-  ctx.nuxt2Context = {
-    app: {
-      router: {
-        beforeEach () {}
-      }
-    },
-    $config: ctx.$config,
-    store: ctx.store
-  }
+
   pluginDef(ctx)
   t.truthy(ctx.components.Json)
-  const { $nuxtStories: state } = ctx.$store.state
-  t.is(state.lang, 'en')
-  t.is(state.storiesDir, 'stories')
+  t.truthy(ctx.$store.state.$nuxtStories)
+})
+
+test('Plugin (static host)', async (t) => {
+  const ctx = pluginCtx()
+  Object.assign(ctx, {
+    nuxt2Context: {
+      app: {
+        router: {
+          beforeEach () {}
+        }
+      },
+      $config: {
+        nuxtStories: {
+          staticHost: true,
+          storiesAnchor: 'stories',
+          storiesDir: 'stories',
+          lang: 'en'
+        }
+      },
+      store: ctx.store
+    }
+  })
+  ctx.$config = ctx.nuxt2Context.$config
+
+  await pluginDef(ctx)
+  t.truthy(ctx.$config.nuxtStories.db)
+  t.is(fetched, '/nuxtStories/stories.db')
 })
