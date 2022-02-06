9333814b4d4ece0ff26e0bb14b2c62db766aacb7
Richard Schloss <richard.e.schloss@gmail.com>
2022-02-05 23:24:36 -0500

    Some surgery performed for StyleEditor concept. To be cleaned up

--- components/Hello.js
+++ components/Hello.js
@@ -1,10 +1,17 @@
 import { h } from 'vue'
 export default {
+  name: 'Hello',
   render () {
+    console.log('style here', this.style)
     return h('div', {
-      style: {
-        color: 'blue'
-      }
+      class: 'hello',
+      style: this.style,
+      editable: true
     }, 'Hi from Hello.js! I am the Hello component. You should after having typed me in!')
+  },
+  computed: {
+    style () {
+      return this.$nuxtStories().value?.styles?.Hello
+    }
   }
 }
